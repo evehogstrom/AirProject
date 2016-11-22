a1110d142e07145a5bed9be1b6635dbb6d8d5efa
User <User@HJZ7J32>
2016-11-22 08:38:04 +0100

    Merge origin/master
    Conflicts:
    	src/myair/Main.java

---
+++ JavaApplication11/src/myair/AirPlanSendOff.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myair;
+import java.lang.Runnable;
+import myair.AirPlanStatus;
+import myair.Airplane;
+import myair.Airplanes;
+/**
+ *
+ * @author User
+ */
+public class AirPlanSendOff implements Runnable {
+   private Airplane  objArplane;
+   private Airplanes  objArplanes;
+   private int nTime;
+   
+    public void run( ) {       
+    
+     sendOffAirPlane(objArplane,objArplanes);   
+        
+    }
+    
+    public void setObjects(Airplane objArplane, Airplanes objArplanes){
+        this.objArplane = objArplane;
+        this.objArplanes = objArplanes;  
+        
+    }
+    
+    public void sendOffAirPlane(Airplane objArplane,Airplanes objArplanes ){
+    {   
+        this.objArplane = objArplane;
+        this.objArplanes = objArplanes;        
+        nTime = 10000;
+        
+    try {                
+                    objArplane.setAirPlaneStatus(AirPlanStatus.TAKEOFF);
+                    objArplanes.listAirplane();
+                    Thread.sleep(nTime);
+                    objArplane.setAirPlaneStatus(AirPlanStatus.FLIGHT);
+                    objArplanes.listAirplane();
+                    Thread.sleep(nTime);
+                    objArplane.setAirPlaneStatus(AirPlanStatus.LANDING);
+                    objArplanes.listAirplane();
+                    Thread.sleep(nTime);
+                    objArplane.setAirPlaneStatus(AirPlanStatus.REFULING);
+                    objArplanes.listAirplane();
+                    Thread.sleep(nTime);
+                    objArplane.setAirPlaneStatus(AirPlanStatus.ONAIRPORT);
+                    objArplanes.listAirplane();
+                    Thread.sleep(nTime);
+                    
+            }   catch (Exception e) {
+                
+            }          
+}
+ 
+   }
+}
